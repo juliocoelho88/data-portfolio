53361fb17bf9eb7a426cbdf3d2418aad5cfa8c5b
Julio <julio.alemao8825@gmail.com>
2024-12-14 10:40:35 -0300

    "Add dataset and exploratory data analysis script".

--- 01_data_analysis/README.md
+++ 01_data_analysis/README.md
@@ -1,12 +1,13 @@
 # Análise Exploratória de Dados
 
-Este projeto realiza uma análise exploratória de um conjunto de dados de vendas no e-commerce. A análise inclui:
-- Estatísticas descritivas dos dados.
+Este projeto realiza uma análise exploratória de um conjunto de dados de e-commerce. A análise inclui:
+- Estatísticas descritivas.
 - Identificação de dados ausentes.
-- Visualizações básicas para entender padrões.
+- Visualizações básicas.
 
 ## Como rodar
-1. Certifique-se de que o dataset `ecommerce_sales.csv` está na pasta `data`.
-2. Execute o script `main.py` localizado em `src`:
+1. Certifique-se de que o dataset `eccomerce_sales.csv` está na pasta `data`.
+2. Substitua as colunas de interesse no script `main.py`.
+3. Execute o script:
    ```bash
    python src/main.py
--- 01_data_analysis/src/main.py
+++ 01_data_analysis/src/main.py
@@ -4,9 +4,14 @@ import seaborn as sns
 
 #Carregar o Dataset
 data_path = "C:/Users/55119/PycharmProjects/data-portfolio/01_data_analysis/data/eccomerce_sales.csv"
+
+print("Carregando o dataset")
 df = pd.read_csv(data_path)
 
-print("Visualizando od dados")
+print("\nInformacoes gerais")
+print(df.info())
+
+print("\nVisualizando od dados")
 print(df.head())
 
 print("\nEstatistica descritiva")
@@ -15,7 +20,18 @@ print(df.describe())
 print("\nVerificar valores nulos")
 print(df.isnull().sum())
 
-sns.histplot(df['column_name'], kde=True) #Substitua o 'column_name' pela coluna que deseja.
-plt.title('Distribuição de Valores - Exemplo')
+plt.figure(figsize=(10,6))
+sns.histplot(df['Price'], kde=True, bins=30) #Substitua o 'column_name' pela coluna que deseja.
+plt.title('Distribuição da Coluna Price')
+plt.xlabel("Valores")
+plt.ylabel("Frequencia")
+plt.show()
+
+# 2. Gráfico de barras para uma variável categórica
+plt.figure(figsize=(10, 6))
+df['High'].value_counts().plot(kind='bar')  # Substitua 'column_category' por uma coluna categórica válida
+plt.title('Frequência por Categoria')
+plt.xlabel('Categorias')
+plt.ylabel('Frequência')
 plt.show()
 
